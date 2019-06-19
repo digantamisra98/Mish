9a0009e42514f2083b8f8246604d4e69732dac19
Diganta Misra <mishradiganta91@gmail.com>
2019-06-19 16:12:45 +0530

    Added PyTorch Implementation

---
+++ Mish/torch/mish.py
@@ -0,0 +1,38 @@
+'''
+Applies the mish function element-wise:
+mish(x) = x * tanh(softplus(x)) = x * tanh(ln(1 + exp(x)))
+'''
+
+# import pytorch
+from torch import nn
+
+# import activation functions
+import Echo.Activation.Torch.functional as Func
+
+class mish(nn.Module):
+    '''
+    Applies the mish function element-wise:
+    mish(x) = x * tanh(softplus(x)) = x * tanh(ln(1 + exp(x)))
+
+    Shape:
+        - Input: (N, *) where * means, any number of additional
+          dimensions
+        - Output: (N, *), same shape as the input
+
+    Examples:
+        >>> m = mish()
+        >>> input = torch.randn(2)
+        >>> output = m(input)
+
+    '''
+    def __init__(self):
+        '''
+        Init method.
+        '''
+        super().__init__()
+
+    def forward(self, input):
+        '''
+        Forward pass of the function.
+        '''
+        return Func.mish(input)
