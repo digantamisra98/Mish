fe1f15c6ee391e1b8f0fb54b4955fd8dd7a9b906
Diganta Misra <mishradiganta91@gmail.com>
2019-07-09 18:45:19 +0530

    Updated Init code
    [ci skip]

---
+++ Mish/Keras/__init__.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from keras.engine.base_layer import Layer
+from keras import backend as K
+
+from .mish import mish
