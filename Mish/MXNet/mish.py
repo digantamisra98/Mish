cf6f7b76a19773b6dfeb416cbbb405ddde2a3ccb
Diganta Misra <mishradiganta91@gmail.com>
2019-08-28 00:28:13 +0530

    Updated Docstring in MXNet implementation
    [ci skip]

--- Mish/MXNet/mish.py
+++ Mish/MXNet/mish.py
@@ -5,6 +5,21 @@ import mxnet as mx
 import mxnet.ndarray as F
 
 class Mish(mx.gluon.HybridBlock):
+'''
+    Mish Activation Function.
+
+    .. math::
+
+        mish(x) = x * tanh(softplus(x)) = x * tanh(ln(1 + e^{x}))
+
+    Shape:
+        - Input: Arbitrary. Use the keyword argument `input_shape`
+        (tuple of integers, does not include the samples axis)
+        when using this layer as the first layer in a model.
+
+        - Output: Same shape as the input.
+
+'''
     def __init__(self):
         super(Mish, self).__init__()
 
